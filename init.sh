f7cdf2e92c62444a92df647e38be5c5f9cd0a452
zen <duzhenquan@eastmoney.com>
2021-08-27 16:11:44 +0800

    new

--- init.sh
+++ init.sh
@@ -2,14 +2,26 @@
 file_name="README.md"
 
 # generate README.md
-echo "\`\`\`text" > $file_name
-tree -N -L 2 | grep -Ev 'img' >> $file_name
-echo "\`\`\`" >> $file_name
+#echo "\`\`\`text" > $file_name
+tree -N -L 2 | grep -Ev 'img|files|init|README' >> $file_name
+#echo "\`\`\`" >> $file_name
 
-git add .
-msg='zendu   '`date "+%Y-%m-%d %H:%M:%S"`;
-git commit -m "${msg}"
-git push 
+
+while read line;do
+  if [[ -n `echo "$line" | grep '\.md'` ]];then
+    suffix=`echo "$line" | awk -F '[「」]' '{print $2}'`
+    fname=`echo "$line"  | grep -Eo '「.*md$'`
+    path="./${suffix}/${fname}"
+    construct="[${fname}](${path})"
+    sed -i "" "s|$fname|$construct|" $file_name
+    # echo "s#"$fname"#"$construct"#" 
+  fi
+done < $file_name
+
+# git add .
+# msg='zendu   '`date "+%Y-%m-%d %H:%M:%S"`;
+# git commit -m "${msg}"
+# git push 
 
 
 
