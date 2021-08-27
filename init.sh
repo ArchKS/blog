aa8ea33c5316df58586460f33a229e6c5ff11ade
zen <duzhenquan@eastmoney.com>
2021-08-27 16:28:01 +0800

    zendu   2021-08-27 16:28:01

--- init.sh
+++ init.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 file_name="README.md"
+file_name_bak="README.md.bak"
+prev_category="";
+
 
 # generate README.md
 echo "\`\`\`text" > $file_name
 tree -N -L 2 | grep -Ev 'img|files|init|README' >> $file_name
 echo "\`\`\`" >> $file_name
+cp $file_name $file_name_bak
 
+while read line;do
+  if [[ -n `echo "$line" | grep '\.md'` ]];then
+    suffix=`echo "$line" | awk -F '[「」]' '{print $2}'`
+    fname=`echo "$line"  | grep -Eo '「.*md$'`
+    path="./${suffix}/${fname}"
+    construct="[${fname}](${path})"
 
-#while read line;do
-#  if [[ -n `echo "$line" | grep '\.md'` ]];then
-#    suffix=`echo "$line" | awk -F '[「」]' '{print $2}'`
-#    fname=`echo "$line"  | grep -Eo '「.*md$'`
-#    path="./${suffix}/${fname}"
-#    construct="[${fname}](${path})"
-#    sed -i "" "s|$fname|$construct|" $file_name
-#    # echo "s#"$fname"#"$construct"#" 
-#  fi
-#done < $file_name
+    if [[ "$prev_category" != "$suffix" ]];then
+      echo "# ${suffix}" >> $file_name;
+      prev_category=$suffix;
+    fi
+
+    if [[ -n $fname ]];then
+      echo "${construct}" >> $file_name
+    fi
+  fi
+done < $file_name_bak
+
+rm -f $file_name_bak
+
 
 git add .
 msg='zendu   '`date "+%Y-%m-%d %H:%M:%S"`;
 git commit -m "${msg}"
 git push 
-
-
 
