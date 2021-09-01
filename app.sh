6d10fc22d9685bc833ceebed1b14afbe8d7e5b25
zen <duzhenquan@eastmoney.com>
2021-09-01 16:40:44 +0800

    zendu   2021-09-01 16:40:44

---
+++ app.sh
@@ -0,0 +1,44 @@
+
+export https_proxy=127.0.0.1:7776
+export http_proxy=127.0.0.1:7776
+
+
+# 判断当前目录
+# 遍历目录下的文章
+
+dirs=`ls -d */ | sed "s#/##g"`
+fileName="README.md"
+rm -f $fileName
+touch $fileName
+
+entry(){
+  echo $1
+  echo $1 >> $fileName;
+}
+
+dateTime=`date "+%Y-%m-%d %H:%M:%S"`
+entry "zendu ${dateTime}" >> $fileName
+
+for dir in ${dirs};
+do
+  entry "## ${dir}" 
+  mks=`ls ${dir}/*.md`
+  for mk in ${mks};
+  do
+    name=`echo $mk | awk -F '[」.]' '{print $2}'`
+    path=$mk
+    link="[$name](./${path})"
+    entry $link
+  done
+  entry " "
+  entry " "
+done
+
+# push to github
+git add .
+msg='zendu   '`date "+%Y-%m-%d %H:%M:%S"`;
+git commit -m "${msg}"
+git push
+
+export https_proxy=
+export http_proxy=
